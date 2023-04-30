8360111ab32ea3b7f6b3a3bec37920e62aefbc3d
Praveen Weerabahu <praveen.weerabahu@gmail.com>
2023-04-30 10:19:55 +0530

    First three exercise of chapter 4

---
+++ chapter 4/exercise_1.py
@@ -0,0 +1,6 @@
+import random
+
+for i in range(10):
+    x = random.random()
+    y = round(random.random() * 10)   #code to create random integers.
+    print(x)
---
+++ chapter 4/exercise_2.py
@@ -0,0 +1,16 @@
+repeat_lyrics()
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+#Traceback (most recent call last):
+#  File "$:\PY4E-Exercises\chapter 4\exercise_2.py", line 1, in <module>
+#    repeat_lyrics()
+#   ^^^^^^^^^^^^^
+#NameError: name 'repeat_lyrics' is not defined
