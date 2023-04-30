73e74702a25eae371a634026a89df5f4a5597b57
Praveen Weerabahu <praveen.weerabahu@gmail.com>
2023-04-30 08:33:21 +0530

    Completed the last exercise of chapter 3

---
+++ chapter 3/exercise_3.py
@@ -0,0 +1,20 @@
+try:
+    score = float(input("Enter your score: "))
+except:
+    print("Please enter a numerical value between 0.0 and 1.0")
+    exit()
+
+if score >= 0 and score <= 1:
+    if(score >= 0.9):
+        print('A')
+    elif(score >= 0.8):
+        print('B')
+    elif(score >= 0.7):
+        print('C')
+    elif(score >= 0.6):
+        print('D')
+    else:
+        print('F')
+
+else:
+    print('Bad score')
